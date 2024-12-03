1ea338d0e6c65f9acc9dffe621221dd3f50c44a8
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2024-12-02 22:21:54 -0800

    updated so more info is logged for steps

--- logger.py
+++ logger.py
@@ -1,39 +1,30 @@
 class Logger:
-    def __init__(self, file_name, verbose=False):
+    def __init__(self, file_name):
         self.file_name = file_name
-        self.verbose = verbose
-        with open(self.file_name, 'w') as file:
-            file.write("")  # Clear the file content
 
     def write_metadata(self, pop_size, vacc_percentage, virus_name, mortality_rate, repro_rate):
-        with open(self.file_name, 'a') as file:
-            file.write(f"Population Size: {pop_size}\n")
-            file.write(f"Vaccination Percentage: {vacc_percentage}\n")
-            file.write(f"Virus Name: {virus_name}\n")
-            file.write(f"Mortality Rate: {mortality_rate}\n")
-            file.write(f"Reproduction Rate: {repro_rate}\n")
-            file.write("=== Start of Simulation ===\n\n")
+        with open(self.file_name, "w") as f:
+            f.write(f"Population Size: {pop_size}\n")
+            f.write(f"Vaccination Percentage: {vacc_percentage}\n")
+            f.write(f"Virus Name: {virus_name}\n")
+            f.write(f"Mortality Rate: {mortality_rate}\n")
+            f.write(f"Reproduction Rate: {repro_rate}\n")
+            f.write("=== Start of Simulation ===\n\n")
 
-    def log_time_step(self, step, new_infections, interactions):
-        with open(self.file_name, 'a') as file:
-            file.write(f"Step {step}:\n")
-            file.write(f"  New Infections: {new_infections}\n")
-            file.write(f"  Total Interactions: {interactions}\n\n")
-
-    def log_interactions(self, infected_id, random_id, did_infect):
-        if self.verbose:
-            with open(self.file_name, 'a') as file:
-                file.write(f"Infected Person {infected_id} interacted with Person {random_id}. ")
-                if did_infect:
-                    file.write("Result: New infection occurred.\n")
-                else:
-                    file.write("Result: No infection occurred.\n")
+    def log_step_summary(self, step, new_infections, interactions, living, dead, vaccinated):
+        with open(self.file_name, "a") as f:
+            f.write(f"Step {step}:\n")
+            f.write(f"  New Infections: {new_infections}\n")
+            f.write(f"  Total Interactions: {interactions}\n")
+            f.write(f"  Total Living: {living}\n")
+            f.write(f"  Total Dead: {dead}\n")
+            f.write(f"  Total Vaccinated: {vaccinated}\n\n")
 
     def log_final_summary(self, pop_size, living, dead, vaccinated, steps):
-        with open(self.file_name, 'a') as file:
-            file.write("\n=== Final Summary ===\n")
-            file.write(f"Total Population: {pop_size}\n")
-            file.write(f"Living: {living}\n")
-            file.write(f"Dead: {dead}\n")
-            file.write(f"Vaccinated: {vaccinated}\n")
-            file.write(f"Total Steps: {steps}\n")
+        with open(self.file_name, "a") as f:
+            f.write("=== Final Summary ===\n")
+            f.write(f"Total Population: {pop_size}\n")
+            f.write(f"Living: {living}\n")
+            f.write(f"Dead: {dead}\n")
+            f.write(f"Vaccinated: {vaccinated}\n")
+            f.write(f"Total Steps: {steps}\n")
